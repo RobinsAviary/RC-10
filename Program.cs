e8a1cbd71165113f5236a7e4d22ac2379e27c133
RobinsAviary <robinsaviary@gmail.com>
2025-06-15 16:54:33 -0400

    interface

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ class Program
 {
     public const uint screenWidth = 96;
     public const uint screenHeight = 64;
+    public const uint windowWidth = 480;
+    public const uint windowHeight = 320;
     public const uint scale = 4;
 
     [STAThread]
@@ -18,14 +20,17 @@ class Program
     {
         Color bg = new Color(167, 193, 145);
         Color fg = new Color(13, 15, 11);
+        Color border = new Color(241, 244, 235);
+        Color pen = fg;
 
         VideoMode mode = new VideoMode();
 
-        mode.Width = screenWidth * scale;
-        mode.Height = screenHeight * scale;
+        mode.Width = (uint)(windowWidth);
+        mode.Height = (uint)(windowHeight);
 
         RenderWindow window = new RenderWindow(mode, "RC-01", Styles.Close);
         RenderTexture render = new RenderTexture(screenWidth, screenHeight);
+        Texture borderTexture = new Texture("resources/border.png");
         render.Clear(bg);
 
         void DrawLine(uint x1, uint y1, uint x2, uint y2)
@@ -34,10 +39,10 @@ class Program
             array.PrimitiveType = PrimitiveType.Lines;
             Vertex v1 = new Vertex();
             Vertex v2 = new Vertex();
-            v1.Position = new Vector2f((float)x1, (float)y1);
-            v2.Position = new Vector2f((float)x2, (float)y2);
-            v1.Color = fg;
-            v2.Color = fg;
+            v1.Position = new Vector2f((float)x1 + .5f, (float)y1 + .5f);
+            v2.Position = new Vector2f((float)x2 + .5f, (float)y2 + .5f);
+            v1.Color = pen;
+            v2.Color = pen;
             
             array.Append(v1);
             array.Append(v2);
@@ -55,19 +60,15 @@ class Program
             DrawLine(x, 0, x, render.Size.Y);
         }
 
-        void Clear(bool fill = false)
+        void PenColor(bool on)
         {
-            Color clearColor;
+            if (on) pen = fg;
+            else pen = bg;
+        }
 
-            if (fill)
-            {
-                clearColor = fg;
-            } else
-            {
-                clearColor = bg;
-            }
-
-            render.Clear(clearColor);
+        void Clear()
+        {
+            render.Clear(pen);
         }
 
         Script scr = new Script();
@@ -76,7 +77,8 @@ class Program
         //Define in-built functions
         scr.Globals["Horizontal"] = (Action<uint>)Horizontal;
         scr.Globals["Vertical"] = (Action<uint>)Vertical;
-        scr.Globals["Clear"] = (Action<bool>)Clear;
+        scr.Globals["Clear"] = (Action)Clear;
+        scr.Globals["PenColor"] = (Action<bool>)PenColor;
 
         scr.DoFile("main.lua");
 
@@ -89,26 +91,44 @@ class Program
 
         window.Closed += Window_Closed;
 
+        object updateFunc = scr.Globals["update"];
+
         while (window.IsOpen)
         {
             window.DispatchEvents();
 
-            scr.Call(scr.Globals["update"]);
+            // Make sure we have an update function
+            if (updateFunc != null)
+            {
+                scr.Call(scr.Globals["update"]);
+            }
 
-            RectangleShape shape = new RectangleShape();
-
-            shape.Position = new Vector2f(2, 8);
-            shape.Size = new Vector2f(5, 5);
-            shape.FillColor = fg;
-
-            render.Draw(shape);
-
+            // Turn the screen into a texture/sprite
             render.Display();
             Sprite sprite = new Sprite();
-            sprite.Texture = render.Texture;
+            Image image = render.Texture.CopyToImage();
+            image.CreateMaskFromColor(bg);
+            sprite.Texture = new Texture(image);
+            sprite.Origin = new Vector2f(screenWidth / 2, screenHeight / 2);
+            sprite.Position = new Vector2f(windowWidth / 2, windowHeight / 2);
+            
             sprite.Scale = new SFML.System.Vector2f(scale, scale);
 
+            uint offset = scale / 2;
+            Sprite offsetSprite = new Sprite(sprite);
+            offsetSprite.Position += new Vector2f((float)offset, (float)offset);
+            offsetSprite.Color = new Color(255, 255, 255, 255 / 4);
+
+            Sprite borderSprite = new Sprite();
+            borderSprite.Texture = borderTexture;
+            borderSprite.Position = new(0, 0);
+            borderSprite.Color = border;
+
+            window.Clear(bg);
+
+            window.Draw(offsetSprite);
             window.Draw(sprite);
+            window.Draw(borderSprite);
 
             window.Display();
         }
